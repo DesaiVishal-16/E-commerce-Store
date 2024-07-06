b6701303130b21f0f9e30ce44b9d79c5873545d4
VishalDesai <desaidesai62230@gmail.com>
2024-07-06 18:44:26 +0530

    Updated Backend

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -145,3 +145,10 @@ import os
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+ALLOWED_HOSTS = ['.vercel.app' , '.now.sh']
+
+import os
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
